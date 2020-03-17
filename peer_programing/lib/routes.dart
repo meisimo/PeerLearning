a2eb13982ceacae926993c31581b78dd01b057d7
checholini <sejrl3@gmail.com>
2020-03-16 23:16:16 -0500

    Added colors and format to the detail view

--- peer_programing/lib/routes.dart
+++ peer_programing/lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peer_programing/src/pages/detalle.dart';
 
 import 'package:peer_programing/src/pages/home_page.dart';
 import 'package:peer_programing/src/pages/recomended_page.dart';
@@ -12,7 +13,7 @@ class Routes{
     '/': (BuildContext context) => new HomePage(),
     '/recomended': (BuildContext context) => new RecomendedPage(),
     '/list_selected': (BuildContext context) => new ListPage(),
-    '/list_proposals': (BuildContext context) => new UserPage(),
+    '/list_proposals': (BuildContext context) => new Detalle(),
     '/user': (BuildContext context) => new UserPage(),
   };
 }
--- peer_programing/lib/src/pages/detalle.dart
+++ peer_programing/lib/src/pages/detalle.dart
@@ -10,17 +10,32 @@ class Detalle extends StatefulWidget {
 class DetalleState extends State<Detalle> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        _detailHeader(),
-        _chipRow(),
-        _textContainer('Descripcion', true),
-        _textContainer('Se dan tutorias en calculo y algebra lineal.', false),
-        _textContainer('Tarifa', true),
-        _textContainer('2000 \$/hora', false),
-        _textContainer('Lugar', true),
-        _textContainer('Parque Simon Bolivar', false),
-      ],
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          _detailHeader(),
+          _chipRow(),
+          Divider(
+            height: 2,
+          ),
+          _textContainer('Descripcion:', true),
+          _textContainer(
+              '\nSe dan tutorias en calculo y algebra lineal.', false),
+          Divider(
+            height: 2,
+          ),
+          _textContainer('Tarifa:', true),
+          _textContainer('\n2000 \$/hora', false),
+          Divider(
+            height: 2,
+          ),
+          _textContainer('Lugar:', true),
+          _textContainer('\nParque Simon Bolivar', false),
+          Divider(
+            height: 2,
+          ),
+        ],
+      ),
     );
   }
 
@@ -39,14 +54,13 @@ class DetalleState extends State<Detalle> {
             Icon(Icons.star),
             Icon(Icons.star),
             Icon(Icons.star_half),
+            Spacer(),
             RaisedButton(
               child: Text('Registrarse'),
-              onPressed: () => {},
+              color: Colors.green,
+              onPressed: () => { }
             )
           ],
-        ),
-        Divider(
-          height: 2,
         ),
       ],
     );
@@ -56,10 +70,18 @@ class DetalleState extends State<Detalle> {
     return Row(
       children: <Widget>[
         Chip(
-          label: Text('Algebra'),
+          label: Text(
+            'Algebra',
+            style: TextStyle(color: Colors.red[400]),
+          ),
+          backgroundColor: Colors.pink[50],
         ),
         Chip(
-          label: Text('Calculo'),
+          label: Text(
+            'Calculo',
+            style: TextStyle(color: Colors.blue[400]),
+          ),
+          backgroundColor: Colors.lightBlue[50],
         ),
       ],
     );
