93d8a20c3be5b19c85f1be46a984d22dfb588dae
checholini <sejrl3@gmail.com>
2020-03-17 18:00:18 -0500

    Added forms to create requests

--- peer_programing/lib/routes.dart
+++ peer_programing/lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peer_programing/src/pages/create_form_page.dart';
 import 'package:peer_programing/src/pages/detalle.dart';
 
 import 'package:peer_programing/src/pages/home_page.dart';
@@ -13,7 +14,7 @@ class Routes{
     '/': (BuildContext context) => new HomePage(),
     '/recomended': (BuildContext context) => new RecomendedPage(),
     '/list_selected': (BuildContext context) => new ListPage(),
-    '/list_proposals': (BuildContext context) => new Detalle(),
+    '/list_proposals': (BuildContext context) => new CreateForm(),
     '/user': (BuildContext context) => new UserPage(),
   };
 }
---
+++ peer_programing/lib/src/pages/create_form_page.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class CreateForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return FormState();
+  }
+}
+
+class FormState extends State<CreateForm> with SingleTickerProviderStateMixin {
+  TabController _formTabPageController;
+  @override
+  void initState() {
+    super.initState();
+    _formTabPageController =
+        TabController(vsync: this, length: 2, initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            Icon(Icons.arrow_back),
+            Text('   Crear publicacion'),
+          ],
+        ),
+        bottom: TabBar(
+          controller: _formTabPageController,
+          tabs: <Widget>[
+            Tab(text: 'Para aprender'),
+            Tab(text: 'Para enseñar'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _formTabPageController,
+        children: <Widget>[_learn(), _teach()],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.save),
+      ),
+    );
+  }
+}
+
+List<String> _items = ['Calculo', 'Fisica', 'Quimica'];
+
+/*class ChipRow {
+  List<String> chipTitles;
+
+  showChipRow() {
+    return Row(
+      children: <Widget>[
+        ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: chipTitles.length,
+            itemBuilder: (context, i) => new Chip(label: Text(chipTitles[i])))
+      ],
+    );
+  }
+}*/
+
+Widget _learn() {
+  List<String> categoriasEscogidas;
+  //ChipRow chips;
+  return Form(
+    autovalidate: true,
+    child: Column(
+      children: <Widget>[
+        //chips.showChipRow(),
+        DropdownButtonFormField(
+          value: null,
+          items: _items.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (String newValue) {
+            categoriasEscogidas.add(newValue);
+            //chips.chipTitles.add(newValue);
+          },
+          decoration: const InputDecoration(
+            hintText: 'Tematicas',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.description),
+            hintText: 'Descripcion',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.monetization_on),
+            hintText: 'Tarifa',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.place),
+            hintText: 'Lugar',
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _teach() {
+ List<String> categoriasEscogidas;
+  //ChipRow chips;
+  return Form(
+    autovalidate: true,
+    child: Column(
+      children: <Widget>[
+        //chips.showChipRow(),
+        DropdownButtonFormField(
+          value: null,
+          items: _items.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (String newValue) {
+            categoriasEscogidas.add(newValue);
+            //chips.chipTitles.add(newValue);
+          },
+          decoration: const InputDecoration(
+            hintText: 'Tematicas',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.description),
+            hintText: 'Descripcion',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.monetization_on),
+            hintText: 'Tarifa',
+          ),
+        ),
+        TextFormField(
+          decoration: const InputDecoration(
+            icon: Icon(Icons.place),
+            hintText: 'Lugar',
+          ),
+        ),
+      ],
+    ),
+  );
+}
